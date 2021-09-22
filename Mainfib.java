ac575759c6f5df257cd3d4b545ed7f19365275a6
Sahil Kataria <59396980+katariasahil@users.noreply.github.com>
2021-09-22 13:13:46 +0530

    Create Mainfib.java

---
+++ Mainfib.java
@@ -0,0 +1,14 @@
+
+public class Mainfib {
+public static void main(String[] args)
+{
+ Fibonacci fib = new Fibonacci();       
+for(int i=0;i<15;i++)
+   {
+   System.out.print(fib.getfib(i)+" ");    
+   }
+} 
+}
+        
+        
+   
