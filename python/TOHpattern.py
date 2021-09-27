1ba6ef539b99d0dab33b7f3aef87809261bc05cb
katariasahil <sahilkataria1999@gmail.com>
2021-09-27 21:10:06 +0530

    pattern

---
+++ python/TOHpattern.py
@@ -0,0 +1,13 @@
+row = 1
+add = 2
+max = 8
+val = 1
+while(row<max):
+    for i in range(1,max-row):
+        print(" ",end=" ")
+    for j in range(1,row+1):
+        print(val,end="   ")
+    print("\n")
+    val= val+add
+    row = row+1
+
